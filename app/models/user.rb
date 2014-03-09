9ccdeafe48c0cd512dc3916b21ce12acefe3975d
Youssef Kababe <youssef.kbe@gmail.com>
2014-03-09 00:26:06 +0000

    Add password confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = %r{\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z}i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { in: 6..18 }
+  validates :password_confirmation, presence: true
 end
